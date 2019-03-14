20fcf90556ca540429505421cf83f8d12dc6c930
Alaa AbdAllah <alaa.abdallah.a97@gmail.com>
2019-03-14 04:34:02 +0200

    new commit

---
+++ Clothes.js
@@ -0,0 +1,60 @@
+
+
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+// Show filtered elements
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+// Hide elements that are not selected
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+
+
+/*=================btn scroll========*/
+
+$("#go-to-top").click(function() {
+    $('html, body').animate({
+        scrollTop: 0
+    }, 1000);
+});
+/*=================btn scroll to Elements========*/
+
+$("[data-target=nav]").click(function() {
+
+  $('html, body').stop().animate({
+    scrollTop: $( $(this).attr('href') ).offset().top
+  }, 1000);
+
+});
+
+
+
